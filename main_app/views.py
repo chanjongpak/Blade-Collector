317e91bf8c2e922a4e41696b433647f2816e1f8d
Chanjong Pak <cjpak@Chanjongs-Air.ht.home>
2022-11-04 17:51:33 -0400

    add project

---
+++ main_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Blade data
+
+class Blade:
+    
+
+def home(request):
+    return HttpResponse('<h1>Hello! Welcome to BladeCollector</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'blades/index.html', {'blades': blades})
